354daa906a4bd53d78ee898a7065fda7ea4ef20a
abigailwambui <abigailw15njuguna@gmail.com>
2019-08-26 13:59:59 +0300

    destination_list_item layout

--- app/src/main/java/com/verisence/zoackadventures/DestinationsArrayAdapter.java
+++ app/src/main/java/com/verisence/zoackadventures/DestinationsArrayAdapter.java
@@ -1,26 +1,26 @@
-package com.verisence.zoackadventures;
-
-import android.content.Context;
-import android.widget.ArrayAdapter;
-
-public class DestinationsArrayAdapter extends ArrayAdapter {
-    private Context mContext;
-    private String[] mDestinations;
-
-    public DestinationsArrayAdapter(Context mContext, int resource, String[] mDestinations){
-        super(mContext, resource);
-        this.mContext = mContext;
-        this.mDestinations = mDestinations;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        String destination = mDestinations[position];
-        return String.format("%s", destination);
-    }
-
-    @Override
-    public int getCount() {
-        return mDestinations.length;
-    }
-}
+//package com.verisence.zoackadventures;
+//
+//import android.content.Context;
+//import android.widget.ArrayAdapter;
+//
+//public class DestinationsArrayAdapter extends ArrayAdapter {
+//    private Context mContext;
+//    private String[] mDestinations;
+//
+//    public DestinationsArrayAdapter(Context mContext, int resource, String[] mDestinations){
+//        super(mContext, resource);
+//        this.mContext = mContext;
+//        this.mDestinations = mDestinations;
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        String destination = mDestinations[position];
+//        return String.format("%s", destination);
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return mDestinations.length;
+//    }
+//}
--- app/src/main/java/com/verisence/zoackadventures/MainActivityAdapter.java
+++ app/src/main/java/com/verisence/zoackadventures/MainActivityAdapter.java
@@ -1,4 +1,46 @@
 package com.verisence.zoackadventures;
 
-public class MainActivityAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
+public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapter.MainViewHolder> {
+    public ArrayList<Destinations> mDestinations = new ArrayList<>();
+    private Context mContext;
+
+    public MainActivityAdapter(Context context, ArrayList<Destinations> mDestinations) {
+        mContext = context;
+        this.mDestinations = mDestinations;
+    }
+
+    @Override
+    public MainActivityAdapter.MainViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from (parent.getContext()).inflate(R.layout.destination_list_item, parent, false);
+        MainViewHolder viewHolder = new MainViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder (MainActivityAdapter.MainViewHolder holder, int position) {
+        holder.bindDestinations(mDestinations.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDestinations.size();
+    }
+
+    public class MainViewHolder extends RecyclerView. ViewHolder implements View
+
+            .OnClickListener {
+        @BindView(R.id.name)
+        TextView
+    }
 }
